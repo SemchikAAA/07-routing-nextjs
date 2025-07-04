b1af698d08d882c89de4b187e09142da26b16b67
Andrii <semtrololo@gmail.com>
2025-07-04 13:30:21 +0200

    The drop-down menu closes when clicking on the link

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -21,13 +21,21 @@ export default function TagsMenu({
       {tagsMenuIsOpen && (
         <ul className={css.menuList}>
           <li className={css.menuItem}>
-            <Link href={`/notes/filter/all`} className={css.menuLink}>
+            <Link
+              href={`/notes/filter/all`}
+              onClick={tagsMenuToggle}
+              className={css.menuLink}
+            >
               All notes
             </Link>
           </li>
           {tags.map((tag) => (
             <li key={tag} className={css.menuItem}>
-              <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+              <Link
+                href={`/notes/filter/${tag}`}
+                onClick={tagsMenuToggle}
+                className={css.menuLink}
+              >
                 {tag}
               </Link>
             </li>
